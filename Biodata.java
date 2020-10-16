bb866d2cfa18761624fb2ca73b111bbd534bc32d
WijayaGanda <64698425+WijayaGanda@users.noreply.github.com>
2020-10-16 09:37:28 +0700

    no message

---
+++ Biodata.java
@@ -0,0 +1,35 @@
+package com.Wijaya;
+
+public class Biodata {
+    public void namaLengkap(){
+        System.out.println("Nama : Wijaya Ganda Prasetyo");
+    }
+    public void jenisKelamin(){
+        System.out.println("Jenis Kelamin : Laki-Laki");
+    }
+    public void Ttl(){
+        System.out.println("TTL : Pasuruan, 23 April 2004");
+    }
+    public void Umur(){
+        System.out.println("Umur : 16 tahun");
+    }
+    public void Alamat(){
+        System.out.println("Alamat : Desa Suwayuwo, Kec Sukorejo, Kab Pasuruan");
+    }
+    public void Hobi(){
+        System.out.println("Hobi : Sepak Bola dan Futsal");
+    }
+    public void Kelas(){
+        System.out.println("Kelas : XI RPL 1");
+    }
+    public void noAbsen(){
+        System.out.println("No absen : 34");
+    }
+    public void Jurusan(){
+        System.out.println("Jurusan : Rekayasa Perangkat Lunak (RPL)");
+    }
+    public void Sekolah(){
+        System.out.println("Sekolah : SMKN 1 Purwosari");
+    }
+
+}
---
+++ Utama.java
@@ -0,0 +1,18 @@
+package com.Wijaya;
+
+public class Utama {
+    public static void main(String[] args) {
+        Biodata biodata = new Biodata();
+
+        biodata.namaLengkap();
+        biodata.jenisKelamin();
+        biodata.Ttl();
+        biodata.Umur();
+        biodata.Alamat();
+        biodata.Hobi();
+        biodata.Kelas();
+        biodata.noAbsen();
+        biodata.Jurusan();
+        biodata.Sekolah();
+    }
+}
